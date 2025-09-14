96b4c99419dc27b9cbfedd51f97f6fca38bbb4da
Sveta Andreeva <89111378147v@yandex.com>
2025-09-14 21:27:55 +0300

    fix: добавила JSDoc,тест на null и исправила проверку принадлежнсти ключа

--- source/transform.js
+++ source/transform.js
@@ -1,19 +1,33 @@
 const transform = function(obj, transformFn) {
+     // Проверка условия: является ли obj массивом
     if (Array.isArray(obj)) {
+    // Для каждого элемента массива рекурсивно вызываем transform
         return obj.map(item => transform(item, transformFn));
     }
-    
+    // Проверка условия: является ли obj обычным объектом (не null)
     if (typeof obj === 'object' && obj !== null) {
+    // Создание нового пустой объект для результатов
         const result = {};
-        
+    // Перебор всех ключей исходного объекта    
         for (const key in obj) {
-            if (Object.prototype.hasOwnProperty.call(obj, key)) {
+    // Проверка, что ключ принадлежит самому объекту
+            if (Object.hasOwn(obj, key)) {
+    // Рекурсивно обрабатываем значение и сохраняем в новый объект
                 result[key] = transform(obj[key], transformFn);
             }
         }
-        
+    // Возвращаем новый объект с преобразованными значениями     
         return result;
     }
-    
+    // Если obj не объект и не массив, применяем функцию преобразования
     return transformFn(obj);
 };
+
+
+
+
+
+
+
+
+
